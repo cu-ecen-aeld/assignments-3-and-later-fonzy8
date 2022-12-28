1e05e056e502a4f3c26286150b557ba5d1ec0c83
fonzy8 <accounts@fonseca.me.uk>
2022-12-28 14:50:45 +0000

    initial commit

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -48,14 +48,17 @@ then
 		exit 1
 	fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
+
+echo "Removing the old writer utility and compiling as a native application"
+cd "../assignments/assignment2"
+make clean
+make
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
+cd "../../finder-app"
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
